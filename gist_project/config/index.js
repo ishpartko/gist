1084570cf0e9360cfc0cdfba062a51ff1d2b0f34
Evan <ishpartko@gmail.com>
2018-07-04 00:19:44 +0300

    fixed path to the static in webpack config

--- gist_project/config/index.js
+++ gist_project/config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../../dist'),
-    assetsSubDirectory: 'gist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/gist/',
 
     /**
      * Source Maps
